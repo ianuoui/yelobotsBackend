4639e20909d1f6eccf085c95c8cadce7d70616e2
ianuoui <ianuoui@gmail.com>
2024-04-08 22:05:12 +0530

    test

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const port =8000;
-
+//const port = 8000;
+const port = process.env.PORT || 5000;
 const app = express();
-app.listen(port, () => console.log(`Server up. Listening on port: ${port}`));
 
 //console.log("Node server is up!");
+app.listen(port, () => console.log(`Server up. Listening on port: ${port}`));
+
+app.get('/api/categories',(req, res) => {
+    //res.send('Get All Categories');
+    res.status(200).json({message: 'Get All Categories'});
+})
