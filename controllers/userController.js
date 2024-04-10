d59632c980881cdf459bf2b4f6c5168b5c906ab5
ianuoui <ianuoui@gmail.com>
2024-04-10 19:55:51 +0530

    test

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,24 +5,44 @@ const User = require('../models/userModel')
 
 
 const registerUser = asyncHandler(async (req,res) => {
-    
+    //res.json({message: 'Register user successful'})
     const { name, email, password } = req.body
+
     if (!name || !email || !password) { 
         res.status(400)
         throw new Error('All fields are mandatory')
     }
     
     const userExists = await User.findOne({ email }) 
+    
     if (userExists) {
         res.status(400)
         throw new Error('User Exists')
     }
 
-    res.json({message: 'Register user successful'})
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    const user = await User.create({name, email, password:hashedPassword})
+
+    if (user){
+        res.status(201).json({ _id: user.id, name: user.name, email: user.email})    
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 const loginUser = asyncHandler(async (req,res) => {
-    res.json({message: 'Login user successful'})
+    //res.json({message: 'Login user successful'})
+    const { email, password } = req.body    
+    const user = await User.findOne({ email })    
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({ _id: user.id, name: user.name, email: user.email })
+    } else {
+        res.status(400)
+        throw new Error('Invalid data')
+    } 
 })
 
 const getCurrentUser = asyncHandler(async (req,res) => {
