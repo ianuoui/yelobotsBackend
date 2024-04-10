5a4379ee7dbb9e328aed83090603b78654a4230f
ianuoui <ianuoui@gmail.com>
2024-04-11 00:35:58 +0530

    test

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,22 +3,22 @@ const Equipment = require('../models/orderModel')
 
 //GET
 const getOrder = asyncHandler(async (req,res) => {
-
+    res.status(200).json({message: 'Code to get all Orders by UserID'});
 })
 
 //POST
 const createOrder = asyncHandler(async (req,res) => {
-
+    res.status(200).json({message: 'Code to create an order'});
 })
 
 //PUT
 const updateOrder = asyncHandler(async (req,res) => {
-
+    res.status(200).json({message: 'Code to update an order'});
 })
 
 //DELETE
 const deleteOrder = asyncHandler(async (req,res) => {
-
+    res.status(200).json({message: 'Code to delete an order'});
 })
 
 module.exports = {getOrder, createOrder, updateOrder, deleteOrder};
