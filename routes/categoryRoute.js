f0fc83e4d798da5d204206ceefc5bb4d9e6d12f5
ianuoui <ianuoui@gmail.com>
2024-04-18 02:42:55 +0530

    test

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -3,24 +3,6 @@ const router = express.Router();
 const { getCategory, createCategory, updateCategory, deleteCategory}  = require('../controllers/categoryController');
 
 
-/*
-router.get('/', (req,res) => {
-    res.status(200).json({message: 'Code to list all Categories'});
-})
-
-router.post('/', (req,res) => {
-    res.status(200).json({message: 'Code to create a new Category'});
-})
-
-router.put('/:id',(req,res) => {
-    res.status(200).json({message: `Code to update a Category : ${req.params.id}`});
-})
-
-router.delete('/:id',(req,res) => {
-    res.status(200).json({message: `Code to delete a Category : ${req.params.id}`});
-})
-*/
-
 router.get('/', getCategory)
 router.post('/', createCategory)
 router.put('/:id', updateCategory)
